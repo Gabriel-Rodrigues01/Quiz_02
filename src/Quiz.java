afdbd7075f55739c77822aea9efb006dcfcc9ebc
Gabriel Rodrigues <gabielrodrigues1234@gmail.com>
2024-05-28 12:32:17 -0300

    Initial commit

---
+++ src/Quiz.java
@@ -0,0 +1,305 @@
+import java.util.Scanner;
+
+public class Quiz {
+    public static void main(String[] args) throws java.lang.Exception{
+        System.out.println("Faculdade: Unifan.");
+        System.out.println("Aluno: Gabriel Rodrigues Pereira.");
+        System.out.println("Professor: Brenno.");
+        System.out.println("##############################################################");
+        System.out.println("#######################   QUIZ_TBBT   ########################");
+        System.out.println("##############################################################");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("01)Quem são os dois personagens principais que compartilham o apartamento no início da série?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Raj e Sheldon");
+        System.out.println("[B] Penny e Amy");
+        System.out.println("[C] Leonard e Sheldon");
+        System.out.println("[D] Howard e Raj");
+
+
+        System.out.println("Digite sua resposta: ");
+        String resposta = scanner.nextLine();
+
+        if(resposta.equals("c")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("02) Qual é o campo de estudo de trabalho de Leonard e Sheldon?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Biologia");
+        System.out.println("[B] Física");
+        System.out.println("[C] Engenharia");
+        System.out.println("[D] Geologia");
+
+
+        System.out.println("Digite sua resposta: ");
+        String resposta2 = scanner2.nextLine();
+
+        if(resposta2.equals("b")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner3 = new Scanner(System.in);
+        System.out.println("Em que andar Sheldon e Leonard moram?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] 4º andar");
+        System.out.println("[B] 1º andar");
+        System.out.println("[C] 6º andar");
+        System.out.println("[D] 3º andar");
+
+
+        System.out.println("Digite sua resposta: ");
+        String resposta3 = scanner3.nextLine();
+
+        if(resposta3.equals("a")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner4 = new Scanner(System.in);
+        System.out.println("04) Qual o trabalho de Rajesh?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A]  Químico");
+        System.out.println("[B] Engenheiro");
+        System.out.println("[C] Médico");
+        System.out.println("[D] Astrofísico");
+
+
+        System.out.println("Digite sua resposta: ");
+        String resposta4 = scanner4.nextLine();
+
+        if(resposta4.equals("D")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner5 = new Scanner(System.in);
+        System.out.println("05) Qual animal Sheldon encontra em sua janela?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Bem-te-vi");
+        System.out.println("[B] Gato malhado");
+        System.out.println("[C] Gaio Azul");
+        System.out.println("[D] Pomba");
+
+
+        System.out.println("Digite sua resposta: ");
+        String resposta5 = scanner5.nextLine();
+
+        if(resposta5.equals("c")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner6 = new Scanner(System.in);
+        System.out.println("06) Qual era o emprego da Penny no começo?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Representande de vendas");
+        System.out.println("[B] Atriz");
+        System.out.println("[C] Entrgadora");
+        System.out.println("[D] Garçonete");
+
+
+        System.out.println("Digite sua resposta: ");
+        String resposta6 = scanner6.nextLine();
+
+        if(resposta6.equals("d")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner7 = new Scanner(System.in);
+        System.out.println("07) Qual o único personagem que não tinha namorda??");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Penny");
+        System.out.println("[B] Raj ");
+        System.out.println("[C] Sheldon");
+        System.out.println("[D] Leonard");
+
+
+        System.out.println("Digite sua resposta: ");
+        String resposta7 = scanner7.nextLine();
+
+        if(resposta7.equals("b")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner8 = new Scanner(System.in);
+        System.out.println("08) Qual a cor do vestido da Amy na premiação do Nobel??");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Preto");
+        System.out.println("[B] Rosa");
+        System.out.println("[C] Verde");
+        System.out.println("[D] Azul");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta8 = scanner8.nextLine();
+
+        if(resposta8.equals("a")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner9 = new Scanner(System.in);
+        System.out.println("09) Quantos filhos Bernadette e Howard tiveram??");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] 2");
+        System.out.println("[B] 6");
+        System.out.println("[C] 2");
+        System.out.println("[D] 14");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta9 = scanner9.nextLine();
+
+        if(resposta9.equals("c")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner10 = new Scanner(System.in);
+        System.out.println("10) Qual o nome da primeira filha de Howard??");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Hamil");
+        System.out.println("[B] Halley");
+        System.out.println("[C] Bernie");
+        System.out.println("[D] mathe");
+
+
+        System.out.println("Digite sua resposta: ");
+        String resposta10 = scanner10.nextLine();
+
+        if(resposta10.equals("b")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner11 = new Scanner(System.in);
+        System.out.println("11) Quantas temporadas tem a série??");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] 10");
+        System.out.println("[B] 9");
+        System.out.println("[C] 4");
+        System.out.println("[D] 12");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta11 = scanner11.nextLine();
+
+        if(resposta11.equals("d")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner12 = new Scanner(System.in);
+        System.out.println("12) Qual número Sheldon considerava perfeito??");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] 73");
+        System.out.println("[B] 53");
+        System.out.println("[C] 13");
+        System.out.println("[D] 69");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta12 = scanner12.nextLine();
+
+        if(resposta12.equals("a")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner13 = new Scanner(System.in);
+        System.out.println("13) O que é \"Bazinga! \"?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Uma expressão popular de Los Angeles");
+        System.out.println("[B] Uma gíria usada por Howard");
+        System.out.println("[C] Uma palavra fictícia inventada por Sheldon");
+        System.out.println("[D] Uma rua de Londres");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta13 = scanner13.nextLine();
+
+        if(resposta13.equals("c")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner14 = new Scanner(System.in);
+        System.out.println("14) científica e jogos de tabuleiro??");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Raj");
+        System.out.println("[B] Sheldon");
+        System.out.println("[C] Leonard");
+        System.out.println("[D] Penny");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta14 = scanner14.nextLine();
+
+        if(resposta14.equals("b")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner15 = new Scanner(System.in);
+        System.out.println("15) Quem é a mulher que Raj finalmente encontra para poder conversar?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Siri");
+        System.out.println("[B] Mulher do Google");
+        System.out.println("[C] Amy");
+        System.out.println("[D] Alexa");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta15 = scanner15.nextLine();
+
+        if(resposta15.equals("a")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+    }
+}
